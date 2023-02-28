52b1120ce75052bfc30d66112e8483778aaa069e
yanmuhamadeev <jonrateta@gmail.com>
2023-02-28 15:06:45 +0500

    add tasks

---
+++ src/labs/lab1/Task5.java
@@ -0,0 +1,11 @@
+package labs.lab1;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); System.out.println("Input name: ");
+        String name = in.nextLine(); System.out.println("Input age: ");
+        int age = in.nextInt(); System.out.println("Input height: ");
+    }
+}
